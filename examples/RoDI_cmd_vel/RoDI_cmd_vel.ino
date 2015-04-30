200e739b5e24da4875309572a3055db078dea244
Gary Servin <garyservin@gmail.com>
2015-04-29 21:51:39 -0400

    Make cmd_vel and ultrasound topics work

--- examples/RoDI_cmd_vel/RoDI_cmd_vel.ino
+++ examples/RoDI_cmd_vel/RoDI_cmd_vel.ino
@@ -4,11 +4,15 @@
  */
 
 #include <Servo.h>
-#include <HCSR04.h>
 
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Range.h>
+
+int lServoPin = 5;
+int rServoPin = 6;
+int TriggerPin = A2;
+int EchoPin = A0;
 
 ros::NodeHandle nh;
 
@@ -16,20 +20,20 @@ sensor_msgs::Range range_msg;
 
 Servo lServo;
 Servo rServo;
-HCSR04 dist(A2, A0); // Trigger, Echo
 
 void messageCb( const geometry_msgs::Twist& cmd_msg) {
+
   if ( cmd_msg.angular.z == 0 && cmd_msg.linear.x == 0 ) {
     move_RoDI(0, 0);
   } else {
-    if ( cmd_msg.angular.z < -5 ) {  // atras
-      move_RoDI(-100, -100);
-    } else if ( cmd_msg.angular.z > 5 ) { // adelante
-      move_RoDI(100, 100);
-    } else if ( cmd_msg.linear.x < -2.5 ) { // derecha
+    if ( cmd_msg.angular.z < 0.0 ) {  // derecha
       move_RoDI(100, -100);
-    } else if ( cmd_msg.linear.x > 2.5 ) {   // izquierda
+    } else if ( cmd_msg.angular.z > 0.0 ) { // izquierda
       move_RoDI(-100, 100);
+    } else if ( cmd_msg.linear.x < 0.0 ) { // atras
+      move_RoDI(-100, -100);
+    } else if ( cmd_msg.linear.x > 0.0 ) {   // adelante
+      move_RoDI(100, 100);
     }
   }
 }
@@ -42,16 +46,17 @@ char frameid[] = "/ultrasound";
 void setup()
 {
   pinMode(13, OUTPUT);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
   nh.initNode();
   nh.advertise(pub_range);
   nh.subscribe(sub_vel);
 
-  dist.begin();
   range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
   range_msg.header.frame_id =  frameid;
-  range_msg.field_of_view = 0.1;  // fake
+  range_msg.field_of_view = 0.79;  // fake
   range_msg.min_range = 0.0;
-  range_msg.max_range = 6.47;
+  range_msg.max_range = 3.0;
 }
 
 long range_time;
@@ -59,8 +64,8 @@ long range_time;
 void loop()
 {
   if ( millis() >= range_time ) {
-    int r = 0;
-    range_msg.range = dist.readCm() / 100;
+  
+    range_msg.range = readCm() / 100.0;
     range_msg.header.stamp = nh.now();
     pub_range.publish(&range_msg);
     range_time =  millis() + 50;
@@ -73,15 +78,26 @@ void move_RoDI(int lSpeed, int rSpeed) {
   if (lSpeed == 0) {
     lServo.detach();
   } else {
-    lServo.attach(5);
+    lServo.attach(lServoPin);
     lServo.write(map(lSpeed, 100, -100, 0, 180));
   }
 
   if (rSpeed == 0) {
     rServo.detach();
   } else {
-    rServo.attach(6);
+    rServo.attach(rServoPin);
     rServo.write(map(rSpeed, -100, 100, 0, 180));
   }
 
+}
+
+int readCm()
+{
+  digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TriggerPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(TriggerPin, LOW);
+
+  return pulseIn(EchoPin, HIGH, 4096) / 2 / 29; // ~35.5cm max
 }
